1216de6805b1cb16ffd32970aed6928858550ab1
cooling75 <jan@laudart.de>
2022-02-05 00:31:39 +0100

    ESP8266 usage plus MQTT

--- electricity/cooling75_holleydtz541-zeba.ino
+++ electricity/cooling75_holleydtz541-zeba.ino
@@ -1,62 +1,105 @@
 
 /*
- *  Application note: Read a Holley DTZ541-ZEBA (2021) electricity meter via
- *  phototransistor + 1k resistor interface and SML protocol
- *  Version 1.0
- *  Copyright (C) 2022  Jan Laudahn https://laudart.de
- *  
- *  credits: 
- *  - Hartmut Wendt  www.zihatec.de for the code base
- *  - user "rollercontainer"
- *           at https://www.photovoltaikforum.com/volkszaehler-org-f131/sml-protokoll-hilfe-gesucht-sml-gt-esp8266-gt-mqtt-t112216-s10.html
- *           
- *  more information about SML protocol:         
- *           http://www.schatenseite.de/2016/05/30/smart-message-language-stromzahler-auslesen/ 
- *           https://wiki.volkszaehler.org/software/sml
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/   
+    Application note: Read a Holley DTZ541-ZEBA (2021) electricity meter via
+    phototransistor + 1k resistor interface and SML protocol
+    Version 1.1
+    Copyright (C) 2022  Jan Laudahn https://laudart.de
 
+    credits:
+    - Hartmut Wendt  www.zihatec.de for the code base
+    - user "rollercontainer"
+             at https://www.photovoltaikforum.com/volkszaehler-org-f131/sml-protokoll-hilfe-gesucht-sml-gt-esp8266-gt-mqtt-t112216-s10.html
 
+    more information about SML protocol:
+             http://www.schatenseite.de/2016/05/30/smart-message-language-stromzahler-auslesen/
+             https://wiki.volkszaehler.org/software/sml
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 #include <SoftwareSerial.h>
 
+// MQTT
+const char* SSID = "";
+const char* PSK = "";
+const char* MQTT_BROKER = "";
+
+String tmpStr;
+char publishValue[10];
+
+char ipAddrPub[15];
+
+// DATA
 byte inByte; //byte to store the serial buffer
 byte smlMessage[1000]; //byte to store the parsed message
 const byte startSequence[] = { 0x1B, 0x1B, 0x1B, 0x1B, 0x01, 0x01, 0x01, 0x01 }; //start sequence of SML protocol
 const byte stopSequence[]  = { 0x1B, 0x1B, 0x1B, 0x1B, 0x1A }; //end sequence of SML protocol
 const byte powerSequence[] =       { 0x77, 0x07, 0x01, 0x00, 0x10, 0x07, 0x00, 0xFF }; //sequence preceeding the current "Real Power" value (2 Bytes)
 const byte consumptionSequence[] = { 0x77, 0x07, 0x01, 0x00, 0x01, 0x08, 0x00, 0xFF }; //sequence predeecing the current "Total power consumption" value (4 Bytes)
+const byte deliveredSequence[] = { 0x77, 0x07, 0x01, 0x00, 0x02, 0x08, 0x00, 0xFF }; // sequence preceeding the delivered to grid power 15 byte to 1 byte (?)
+const byte vendorSequence[] = { 0x77, 0x07, 0x01, 0x00, 0x60, 0x32, 0x01, 0x01 }; // sequence preceeding the vendor shortname, 6 bytes to 3 byte in ASCII, here "HLY"
+const byte uptimeSequence[] = { 0x07, 0x01, 0x00, 0x62, 0x0A, 0xFF, 0xFF }; // 5 bytes to 4 byte uptime
 int smlIndex; //index counter within smlMessage array
 int startIndex; //start index for start sequence search
 int stopIndex; //start index for stop sequence search
 int stage; //index to maneuver through cases
 byte power[8]; //array that holds the extracted 4 byte "Wirkleistung" value
 byte consumption[8]; //array that holds the extracted 4 byte "Gesamtverbrauch" value
+byte delivered[1];
+byte uptime[8];
+unsigned long uptimeTotal;
+unsigned long deliveredTotal;
 unsigned long currentpower; //variable to hold translated "Wirkleistung" value
 unsigned long currentconsumption; //variable to hold translated "Gesamtverbrauch" value
 float currentconsumptionkWh; //variable to calulate actual "Gesamtverbrauch" in kWh
+float uptimeTotalDays; // uptime in days
 
-SoftwareSerial MeterSerial(4, 3, true); // RX, TX, inverted mode(!)
+int pin_d2 = 4;
 
-//#define _debug_msg
+SoftwareSerial MeterSerial(pin_d2, 3, true); // RX, TX, inverted mode(!)
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+// #define _debug_msg
 
 void setup() {
-// bring up serial ports
-  Serial.begin(115200);   // debug via USB 
+  // bring up serial ports
+  Serial.begin(115200);   // debug via USB
   MeterSerial.begin(9600);  // meter via RS485
-
+  setup_wifi();
+  client.setServer(MQTT_BROKER, 1883);
   Serial.println("Waiting for data");
+}
+
+void setup_wifi() {
+  WiFi.begin(SSID, PSK);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100);
+  }
+
+  Serial.println(WiFi.localIP());
+}
+
+String IpAddress2String(const IPAddress& ipAddress)
+{
+  return String(ipAddress[0]) + String(".") +
+         String(ipAddress[1]) + String(".") +
+         String(ipAddress[2]) + String(".") +
+         String(ipAddress[3]);
 }
 
 void loop() {
@@ -74,8 +117,15 @@ void loop() {
       findConsumptionSequence(); //look for consumption sequence and exctract
       break;
     case 4:
-      publishMessage(); // do something with the result
-      break;   
+      findDeliveredSequence(); // look for power send to grid
+      break;
+    case 5:
+      findUptime(); // look for uptime of power meter
+      break;
+    case 6:
+      publishMessage(); // send out via MQQTT
+      // delay(5000); // send out data max. every 5 seconds
+      break;
   }
 }
 
@@ -84,16 +134,16 @@ void findStartSequence() {
   while (MeterSerial.available())
   {
     inByte = MeterSerial.read(); //read serial buffer into array
-    
+
     if (inByte == startSequence[startIndex]) //in case byte in array matches the start sequence at position 0,1,2...
     {
       smlMessage[startIndex] = inByte; //set smlMessage element at position 0,1,2 to inByte value
       startIndex++;
       if (startIndex == sizeof(startSequence)) //all start sequence values have been identified
       {
-        #ifdef _debug_msg
+#ifdef _debug_msg
         Serial.println("Match found - Start Sequence");
-        #endif
+#endif
         stage = 1; //go to next case
         smlIndex = startIndex; //set start index to last position to avoid rerunning the first numbers in end sequence search
         startIndex = 0;
@@ -119,9 +169,9 @@ void findStopSequence() {
       stopIndex++;
       if (stopIndex == sizeof(stopSequence))
       {
-        #ifdef _debug_msg
+#ifdef _debug_msg
         Serial.println("Match found - Stop Sequence");
-        #endif
+#endif
         stage = 2;
         stopIndex = 0;
       }
@@ -134,25 +184,25 @@ void findStopSequence() {
 
 void findPowerSequence() {
   byte temp; //temp variable to store loop search data
- startIndex = 0; //start at position 0 of exctracted SML message
- 
-for(int x = 0; x < sizeof(smlMessage); x++){ //for as long there are element in the exctracted SML message
+  startIndex = 0; //start at position 0 of exctracted SML message
+
+  for (int x = 0; x < sizeof(smlMessage); x++) { //for as long there are element in the exctracted SML message
     temp = smlMessage[x]; //set temp variable to 0,1,2 element in extracted SML message
     if (temp == powerSequence[startIndex]) //compare with power sequence
     {
       startIndex++;
       if (startIndex == sizeof(powerSequence)) //in complete sequence is found
       {
-        for(int y = 0; y< 3; y++){ //read the next 2 bytes (the actual power value)
-          power[y] = smlMessage[x+y+8]; //store into power array
-          #ifdef _debug_msg
+        for (int y = 0; y < 3; y++) { //read the next 2 bytes (the actual power value)
+          power[y] = smlMessage[x + y + 8]; //store into power array
+#ifdef _debug_msg
           Serial.print(String(power[y], HEX));
           Serial.print(" ");
-          #endif        
+#endif
         }
-        #ifdef _debug_msg
+#ifdef _debug_msg
         Serial.println();
-        #endif
+#endif
         stage = 3; // go to stage 3
         startIndex = 0;
       }
@@ -161,32 +211,31 @@ for(int x = 0; x < sizeof(smlMessage); x++){ //for as long there are element in 
       startIndex = 0;
     }
   }
-   currentpower = (power[0] << 8 | power[1] << 0); //merge 2 bytes into single variable to calculate power value
+  currentpower = (power[0] << 8 | power[1] << 0); //merge 2 bytes into single variable to calculate power value
 }
-
 
 void findConsumptionSequence() {
   byte temp;
- 
+
   startIndex = 0;
-for(int x = 0; x < sizeof(smlMessage); x++){
+  for (int x = 0; x < sizeof(smlMessage); x++) {
     temp = smlMessage[x];
     if (temp == consumptionSequence[startIndex])
     {
       startIndex++;
       if (startIndex == sizeof(consumptionSequence))
       {
-        for(int y = 0; y < 4; y++){
+        for (int y = 0; y < 4; y++) {
           // get 4 bytes containing the total consumption
-          consumption[y] = smlMessage[x+y+19];
-          #ifdef _debug_msg
+          consumption[y] = smlMessage[x + y + 19];
+#ifdef _debug_msg
           Serial.print(String(consumption[y], HEX));
           Serial.print(" ");
-          #endif
+#endif
         }
-        #ifdef _debug_msg
+#ifdef _debug_msg
         Serial.println();
-        #endif
+#endif
         stage = 4;
         startIndex = 0;
       }
@@ -196,42 +245,138 @@ for(int x = 0; x < sizeof(smlMessage); x++){
     }
   }
 
-  #ifdef _debug_msg
-  for(int x=0;x<sizeof(smlMessage);x++){
-  Serial.print(smlMessage[x],HEX);
-  Serial.print(" ");
+#ifdef _debug_msg
+  for (int x = 0; x < sizeof(smlMessage); x++) {
+    Serial.print(smlMessage[x], HEX);
+    Serial.print(" ");
   }
-  #endif
- 
-   currentconsumption = consumption[0];
-   currentconsumption <<= 8;
-   currentconsumption += consumption[1];
-   currentconsumption <<= 8;
-   currentconsumption += consumption[2];
-   currentconsumption <<= 8;
-   currentconsumption += consumption[3];
+  delay(30000); // wait for 30 seconds while in debug mode
+#endif
 
-   // scale 10⁻1 and unit is Wh => factor 10.000 for kWh
-   currentconsumptionkWh = (float)currentconsumption / 10000;
-   
+  currentconsumption = consumption[0];
+  currentconsumption <<= 8;
+  currentconsumption += consumption[1];
+  currentconsumption <<= 8;
+  currentconsumption += consumption[2];
+  currentconsumption <<= 8;
+  currentconsumption += consumption[3];
+
+  // scale 10⁻1 and unit is Wh => factor 10.000 for kWh
+  currentconsumptionkWh = (float)currentconsumption / 10000;
+
 }
 
+void findDeliveredSequence() {
+  byte temp;
+  startIndex = 0;
+  for (int x = 0; x < sizeof(smlMessage); x++) {
+    temp = smlMessage[x];
+    if (temp == deliveredSequence[startIndex]) {
+    startIndex++;
+    if (startIndex == sizeof(deliveredSequence)) {
+        for (int y = 0; y < 1; y++) {
+          delivered[y] = smlMessage[x + y + 1];
+        }
+        startIndex = 0;
+        stage = 5;
+      }
+    } else {
+      startIndex = 0;
+    }
+  }
+  // maybe the value extends when power is delivered to grid(?)
+  deliveredTotal = delivered[0];
+}
+
+void findUptime() {
+  byte temp;
+  startIndex = 0;
+  for (int x = 0; x < sizeof(smlMessage); x++) {
+    temp = smlMessage[x];
+    if (temp == uptimeSequence[startIndex]) {
+      startIndex++;
+      if (startIndex == sizeof(uptimeSequence)) {
+        Serial.print("Uptime (bytes): ");
+        for (int y = 0; y < 4; y++) {
+          uptime[y] = smlMessage[x + y + 5];
+          Serial.print(String(uptime[y], HEX));
+          Serial.print(" ");
+        }
+        startIndex = 0;
+        stage = 6;
+      }
+    } else {
+      startIndex = 0;
+    }
+  }
+  // combine to uptimeTotal
+  uptimeTotal = uptime[0];
+  uptimeTotal <<= 8;
+  uptimeTotal += uptime[1];
+  uptimeTotal <<= 8;
+  uptimeTotal += uptime[2];
+  uptimeTotal <<= 8;
+  uptimeTotal += uptime[3];
+
+  uptimeTotalDays = (float)uptimeTotal / 86400;
+}
 
 void publishMessage() {
-//Wirkleistung
-Serial.print("Real Power: ");
-Serial.print(currentpower);
-Serial.println(" W");
-// Gesamtverbrauch
-Serial.print("Total Consumption: ");
-Serial.print(currentconsumptionkWh);
-Serial.println(" kWh");
+#ifdef _debug_msg
+  // Wirkleistung
+  Serial.print("Real Power: ");
+  Serial.print(currentpower);
+  Serial.println(" W");
+  // Gesamtverbrauch
+  Serial.print("Total Consumption: ");
+  Serial.print(currentconsumptionkWh);
+  Serial.println(" kWh");
+  // power delivered to grid
+  Serial.print("Total delivered: ");
+  Serial.print(deliveredTotal);
+  Serial.println(" kWh");
+  // Uptime
+  Serial.print("Uptime (sec.): ");
+  Serial.print(uptimeTotal);
+  Serial.println(" sec.");
+  Serial.print("Uptime (days): ");
+  Serial.print(uptimeTotalDays);
+  Serial.println(" days");
+#endif
+  
+  // start MQTT
+  if (!client.connected()) {
+    while (!client.connected()) {
+      client.connect("ESP8266Client");
+      delay(100);
+    }
+  }
 
-// clear the buffers
+  client.loop();
+#ifndef _msg_debug
+  client.publish("/home/commodity/electricity/consumedDeciWatt", int2charArray(currentconsumption));
+  client.publish("/home/commodity/electricity/deliveredkwh", int2charArray(deliveredTotal));
+  client.publish("/home/commodity/electricity/currentWatt", int2charArray(currentpower));
+  client.publish("/home/commodity/electricity/uptime", int2charArray(uptimeTotal));
+  // tmpStr = IpAddress2String(WiFi.localIP());
+  // tmpStr.toCharArray(ipAddrPub, 15);
+  // client.publish("/home/commodity/electricity/ip", ipAddrPub);
+#endif
+
+  // clear the buffers
   memset(smlMessage, 0, sizeof(smlMessage));
   memset(power, 0, sizeof(power));
   memset(consumption, 0, sizeof(consumption));
-//reset case
+  memset(delivered, 0, sizeof(delivered));
+  memset(uptime, 0, sizeof(uptime));
+  //reset case
   smlIndex = 0;
   stage = 0; // start over
 }
+
+char* int2charArray(const unsigned long value){
+  tmpStr = "";
+  tmpStr += value;
+  tmpStr.toCharArray(publishValue, 10);
+  return publishValue;
+  }
